e97b037a09118db8e797482b030abfab2baa8d4d
Dude112113 <curtisdude12@gmail.com>
2019-06-23 02:45:28 +0200

    Added comments to explain stuff

--- CodeDog/__init__.py
+++ CodeDog/__init__.py
@@ -5,14 +5,25 @@ from .window import Window
 
 
 def main():
+	# much like `System.out.println()` in java
 	print("Opening window.")
+	# create an app (all i know is that is needed)
 	app = QApplication(sys.argv)
 	app.setStyle("Fusion")
 
+	# create a window, from are custom class in window.py
 	window = Window()
+	# set window title
 	window.setWindowTitle("Window")
+	# set the smallest size the window can be realized to
 	window.setMinimumSize(550, 600)
+	# initialize the ui, we defined this function in window.py
 	window.initUI()
+	# then show the window
 	window.show()
 
+	# `sys.exit()` is same as `exit()`
+	# `app.exec_()` will run the app and wait until all windows
+	# are closed, and then return an error code that is
+	# normally `0` for no error's
 	sys.exit(app.exec_())
--- CodeDog/style.py
+++ CodeDog/style.py
@@ -2,29 +2,56 @@ from glob import iglob
 import os
 
 
+# path of the folder containing all the styles
+# `__file__` is a python magic variable that in this
+# case is `[PATH_TO_PROJECT_DIR]\CodeDog\style.py`
+# then we get the `dirname()` so it removes `style.py` from name
+# then joins `style` to the path for the folder
 STYLE_PATH = os.path.join(os.path.dirname(__file__), "style")
+# keep the styles around so we don't need to get them from files
+# all the time
 _styleCache = {}
 
 
 def getStyle(name: str):
+	# if name is in are cache
 	if name in _styleCache:
+		# return the cached style
 		return _styleCache[name]
+	# create a variable to store the style in
 	style = ""
+	# add the `name` to the path ([PATH_TO_PROJECT_DIR]\CodeDog\style\[NAME])
 	path = os.path.join(STYLE_PATH, name)
+	# if it does not exist
 	if not os.path.exists(path):
+		# raise an error saying that it failed to find
 		raise FileNotFoundError(f"Failed to find file/folder at '{path}'")
-	if os.path.isfile(path):
+	# if its a file
+	elif os.path.isfile(path):
+		# open it up
 		with open(path) as f:
+			# read all the content and store it in variable `style`
 			style = f.read()
+	# if its a directory
 	elif os.path.isdir(path):
+		# find every file ending with `.css`
 		for filePath in iglob(os.path.join(path, "*.css"), recursive=True):
+			# open it up
 			with open(filePath) as f:
+				# add the content to the style `variable`
+				# joining multiple together into one large style
 				style += "\n\n%s" % f.read()
 	else:
+		# raise an error saying that it failed to find
+		# but in this case its an unsupported type
+		# for example a symlink
 		raise FileNotFoundError(f"Failed to find file/folder at '{path}'")
+	# save the style in the cache
 	_styleCache[name] = style.strip()
+	# return the resulting style
 	return style
 
 
 def clearStyleCache():
+	# empty the dictionary
 	_styleCache.clear()
